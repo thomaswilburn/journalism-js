b7f569a756f284a02027be0c006f575f7a1cd6a8
Thomas Wilburn <news@thomaswilburn.net>
2017-09-02 11:40:29 -0700

    Replace JSDOM with Cheerio

--- scraping-by/scraper.js
+++ scraping-by/scraper.js
@@ -1,5 +1,5 @@
 var async = require("async");
-var jsdom = require("jsdom");
+var cheerio = require("cheerio");
 var request = require("request");
 
 var fs = require("fs");
@@ -15,27 +15,28 @@ if (!fs.existsSync("downloads")) fs.mkdirSync("downloads");
 console.log("Requesting table of contents");
 request(tocURL, function(err, response, body) {
   //create a document and look for links to "document.cfm"
-  var document = jsdom.html(body);
-  var links = document.querySelectorAll("a[href*='document.cfm']");
+  var $ = cheerio(body);
+  var links = $.find("a[href*='document.cfm']");
   console.log("Found document links: ", links.length);
   
   //process each link asynchronously
-  async.each(links, function(a, c) {
+  async.each(links.toArray(), function(a, c) {
     //download the page
-    var page = url.resolve(searchURL, a.getAttribute("href"));
+    var $a = cheerio(a);
+    var page = url.resolve(searchURL, $a.attr("href"));
     console.log("Requesting page:", page);
     
     request(page, function(err, response, body) {
-      var document = jsdom.html(body);
+      var $page = cheerio(body);
       //find the hidden input
-      var input = document.querySelector("input[name='src']");
+      var input = $page.find("input[name='src']");
       
       //if missing, exit early
-      if (!input) return c();
+      if (!input.length) return c();
       
       //get URL and download the file
-      var download = url.resolve(domain, decodeURIComponent(input.value));
-      var title = a.innerHTML.replace(/\W/g, "");
+      var download = url.resolve(domain, decodeURIComponent(cheerio(input).val()));
+      var title = $a.html().replace(/\W/g, "").slice(0, 60) + "-" + Date.now();
       var output = fs.createWriteStream("downloads/" + title + ".pdf")
       console.log("Downloading file: ", download);
       var r = request(download, c).pipe(output);
